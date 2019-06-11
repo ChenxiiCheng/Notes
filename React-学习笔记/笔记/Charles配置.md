344c94372438bb42d1fdeba7a6929dcdea05039a
ChenxiiCheng <chenxic1011@gmail.com>
2019-06-11 22:09:50 +0800

    added notes

--- React-学习笔记/笔记/Charles配置.md
+++
@@ -1,6 +0,0 @@
-# Charles配置
-
-**1. Local Map里面 Host填localhost.charlesproxy.com**
-
-**2. 浏览器中使用<http://localhost.charlesproxy.com:3000/>**
-
--- React-学习笔记/简书实战详细分解代码/7-1 页面顶部空白解决方法.js
+++
@@ -1,9 +0,0 @@
-//页面顶部空白
-发现页面顶部有一定的空白，通过查看发现是body:有margi:8px
-解决方案：我在根目录style.css，我没有把它写成style.js，使用styled-components，
-我直接在style.css中：
-body {
-  margin: 0;
-}
-视频里是把根目录的style.css改成style.js，然后网上搜reset.css，把内容放进这个style.js中
-同时在style.js中使用了styled-components,注意这个第三方库改了写法
